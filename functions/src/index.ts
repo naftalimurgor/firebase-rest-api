259115b2ea0a55aefb36ae491bd997ac2c8f115f
naftalimurgor <murgornaftali@gmail.com>
2023-09-24 14:41:31 +0300

    Hello world function!

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,13 +7,17 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+// https://stackoverflow.com/questions/76434349/firebase-cloud-functions-v2-error-when-deploying
+import {setGlobalOptions} from "firebase-functions/v2/options";
+setGlobalOptions({maxInstances: 10});
+
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
